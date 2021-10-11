372f48e315f8893c0e1ea8a4adb32bbdcd1f5a9b
Ranitha <ranitha.m@gmail.com>
2021-10-11 16:36:12 +1100

    Started REST development

--- config/mindbody.php
+++ config/mindbody.php
@@ -21,23 +21,6 @@ return [
                 'password' => env('MINDBODY_USERPASSWORD', env('MINDBODY_SOURCEPASSWORD')),
             ],
 
-            'endpoints' => [
-                'AppointmentService' => "https://api.mindbodyonline.com/0_5_1/AppointmentService.asmx?WSDL",
-                'ClassService'       => "https://api.mindbodyonline.com/0_5_1/ClassService.asmx?WSDL",
-                'ClientService'      => "https://api.mindbodyonline.com/0_5_1/ClientService.asmx?WSDL",
-                'SaleService'        => "https://api.mindbodyonline.com/0_5_1/SaleService.asmx?WSDL",
-                'SiteService'        => "https://api.mindbodyonline.com/0_5_1/SiteService.asmx?WSDL",
-                'StaffService'       => "https://api.mindbodyonline.com/0_5_1/StaffService.asmx?WSDL",
-            ],
-
-            'services' => [
-                'AppointmentService'    => \Nlocascio\Mindbody\MBOSoap\Appointment_Service::class,
-                'ClassService'          => \Nlocascio\Mindbody\MBOSoap\Class_Service::class,
-                'ClientService'         => \Nlocascio\Mindbody\MBOSoap\Client_Service::class,
-                'SaleService'           => \Nlocascio\Mindbody\MBOSoap\Sale_Service::class,
-                'SiteService'           => \Nlocascio\Mindbody\MBOSoap\Site_Service::class,
-                'StaffService'          => Nlocascio\Mindbody\MBOSoap\Staff_Service::class,
-            ],
         ]
     ],
 ];
