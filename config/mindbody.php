353c94b473c5cf0b7d8f262b6e3ab95b08492763
Ranitha <ranitha.m@gmail.com>
2021-10-14 13:47:08 +1100

    Bugfix and minor composer file updates (package discovery)

--- config/mindbody.php
+++ config/mindbody.php
@@ -4,7 +4,7 @@ return [
   "debug" => env("MINDBODY_DEBUG", false),
   "site_id" => env("MINDBODY_SITEIDS"),
   "source_credentials" => [
-    "username" => "_" + env("MINDBODY_SOURCENAME"),
+    "username" => "_" . env("MINDBODY_SOURCENAME"),
     "password" => env("MINDBODY_SOURCEPASSWORD"),
   ],
   "source_name" => env("MINDBODY_SOURCENAME"),
